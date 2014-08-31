76fe48a14d77b08fd8ee2f2d07691b2b94af126a
Jakub Zolcik <j.zolcik@allblue.pl>
2014-08-31 13:48:57 +0300

    Dynamic Sort operators.

--- source/blender/editors/object/object_ops.c
+++ source/blender/editors/object/object_ops.c
@@ -247,6 +247,9 @@ void ED_operatortypes_object(void)
 	WM_operatortype_append(OBJECT_OT_lod_remove);
 
 	WM_operatortype_append(OBJECT_OT_vertex_random);
+    
+	WM_operatortype_append(OBJECT_OT_dsort);
+	WM_operatortype_append(OBJECT_OT_dsort_free);
 }
 
 void ED_operatormacros_object(void)
